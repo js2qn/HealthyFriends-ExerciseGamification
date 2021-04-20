0af43a36d64e9ee24819365eba2941ec28a5cd33
Jayden Sharpe <littlejmics@gmail.com>
2021-04-20 14:14:01 -0400

    passed travis test

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'healthyfriendsdataB', 
-        'USER': 'postgres', 
-        'PASSWORD': '!200ssenevigr0F',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
