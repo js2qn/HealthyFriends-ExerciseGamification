f43e8639dddda16e31f2842abe252e2d8d372a87
nicolechheng <71405252+nicolechheng@users.noreply.github.com>
2021-04-25 14:41:11 -0400

    Update views.py

--- healthyfriends/views.py
+++ healthyfriends/views.py
@@ -80,7 +80,7 @@ def updatePoints(request):
 
 def achievementsView(request):
     #pts = Points.objects.filter(user=request.user).count()
-    achievements_ct = Points.objects.filter(user=request.user).count()
+    achievements_ct = Points.objects.filter(user=request.user)
     achievements = Workouts.objects.filter(user=request.user).order_by('-date')
     return render(request, 'healthyfriends/achievements.html', {'achievements_ct':achievements_ct, 'achievements':achievements})
 
