49fb427f731b0b23fd042a2fecf3c3b2318b0fed
nicolechheng <71405252+nicolechheng@users.noreply.github.com>
2021-05-03 01:40:56 -0400

    Update views.py

--- healthyfriends/views.py
+++ healthyfriends/views.py
@@ -105,13 +105,15 @@ def leaderboardView(request):
         ranking.append(rank)
         rank = rank + 1
         x.rank = rank
+        if(rank > Points.objects.count()):
+            break;
 
     for i in us:
         if(i not in user_list):
             point_users.append(i)
 
-    while(len(pts) < user_ct):
-        pts.append(0)
+    #while(len(pts) < user_ct):
+    #    pts.append(0)
 
     return render(request, 'healthyfriends/leaderboard.html', {'user_ct':user_ct, 'users':user_list, 'rank':ranking, 'pts':pts})
 #class achievementsView(TemplateView):
